9ab06d805d12636a6e5746fae67715d4fcf14112
Bulletz.Dev <gabri.proietto2007@gmail.com>
2025-05-05 07:07:06 +0200

    initialized battleship with spring

---
+++ src/battleShips/src/test/java/it/proietto/battleShips/BattleShipsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.proietto.battleShips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleShipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
